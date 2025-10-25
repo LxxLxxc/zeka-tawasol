755b4ae8b9f1ba0cb4b14c353cfb0ff28fabc5f4
LxxLxxc <t713497420@gmail.com>
2025-10-25 23:49:56 +0300

    Update Comment.js

--- components/Comment.js
+++ components/Comment.js
@@ -1,7 +1,12 @@
-export default function Comment({ author, text }) {
+import VerifiedBadge from './VerifiedBadge';
+
+export default function Comment({ author, role, text }) {
   return (
     <div style={{ padding: '5px 10px', borderTop: '1px solid #eee' }}>
-      <p><strong>{author}</strong>: {text}</p>
+      <p>
+        <strong>{author}</strong>
+        <VerifiedBadge role={role} />: {text}
+      </p>
     </div>
   );
 }
